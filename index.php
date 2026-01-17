c9416adc85e0c4bc8a59cd9941b5c081b4345b1d
Tanvir284 <ruhittanvir14@gmail.com>
2026-01-17 13:14:51 +0600

    Add Railway deployment configuration

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * E-Shop Bangladesh - Main Entry Point
+ * Redirects to the customer shop page
+ */
+
+// Redirect to the main shop page
+header('Location: Management/Customer/MVC/html/index.html');
+exit;
+?>
